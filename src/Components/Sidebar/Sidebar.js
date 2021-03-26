82a79c20c5c47f79382908ad9c79c04547147175
Nikunj018 <53224032+Nikunj018@users.noreply.github.com>
2021-03-26 12:58:08 +0530

    sidebar completed

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,23 +1,28 @@
 import React from 'react'
-
+import './Sidebar.css'
 const Sidebar = () => {
   return (
-    <div className="container-fluid sidebar">
-    
-        <ul className="social-icon-list">
-            <li>
-                <a href="/" target="_blank"><i className="fab fa-twitter-square"></i></a>
-            </li>
-            <li>
-                <a href="/" target="_blank"><i class="fab fa-linkedin"></i></a>
-            </li>
-            <li>
-                <a href="/" target="_blank"><i className="fab fa-facebook"></i></a>
-            </li>
-            <li>
-                <a href="/" target="_blank"><i class="fab fa-youtube"></i></a>
-            </li>
-        </ul>
+    <div className="container-fluid">
+        <div className="row">
+
+            <div className="d-none d-sm-block sidebar">
+                <ul className="social-icon-list">
+                    <li className="list">
+                        <a href="https://twitter.com/" target="_blank" rel="noreferrer"><i className="fab fa-twitter-square"></i></a>
+                    </li>
+                    <li className="list">
+                        <a href="https://in.linkedin.com/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
+                    </li>
+                    <li className="list">
+                        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i class="fab fa-facebook-square"></i></a>
+                    </li>
+                    <li className="list">
+                        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer"><i class="fab fa-youtube-square"></i></a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
     </div>
   )
 }
